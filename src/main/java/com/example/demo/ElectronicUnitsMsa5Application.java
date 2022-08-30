889d2364b4ff2a3883989e6c0296f38ae4f1fadf
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-30 12:03:30 +0530

    Spring Rest Api app is workinf as expected

--- src/main/java/com/example/demo/ElectronicUnitsMsa5Application.java
+++ src/main/java/com/example/demo/ElectronicUnitsMsa5Application.java
@@ -6,14 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ElectronicUnitsMsa5Application {
 
-	@GetMapping("/user")
-	public String print() {
-		return "Hello I am Connected";
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicUnitsMsa5Application.class, args);
 	}
