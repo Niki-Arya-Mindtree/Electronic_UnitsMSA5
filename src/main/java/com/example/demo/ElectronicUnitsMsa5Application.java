01c710088a36d2c791d9a59cab2a0c902994b29f
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-30 10:44:22 +0530

    Initail CommitEmpty Spring Project Created

---
+++ src/main/java/com/example/demo/ElectronicUnitsMsa5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicUnitsMsa5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicUnitsMsa5Application.class, args);
+	}
+
+}
