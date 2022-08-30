a736cb8b9e86128fd39da91333d0001a63009d88
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-30 10:57:12 +0530

    Code Deployed to test the connection with git->jenkins & Azure ->Host

--- src/main/java/com/example/demo/ElectronicUnitsMsa5Application.java
+++ src/main/java/com/example/demo/ElectronicUnitsMsa5Application.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ElectronicUnitsMsa5Application {
 
+	@GetMapping("/user")
+	public String print() {
+		return "Hello I am Connected";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicUnitsMsa5Application.class, args);
 	}
